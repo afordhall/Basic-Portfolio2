8521bc6110ef8622d036cd6458943125a7e84fbc
Athagod <afordhal10@mail.bw.edu>
2020-01-14 22:14:44 -0500

    color changes

---
+++ index.js
@@ -0,0 +1,17 @@
+// var nameInput = document.querySelector("#name");
+// var emailInput = document.querySelector("#email");
+// var msgInput = document.querySelector("#message");
+// var submitButton = document.querySelector("#submit")
+
+// submitButton.addEventListener("click", function(event) {
+//     event.preventDefault(); 
+// })
+
+// var user = {
+//     name: nameInput.value.trim(),
+//     email: emailInput.value.trim(),
+//     message: msgInput.value.trim(),
+// };
+
+// console.log(user);
+// localStorage.setItem("user", user)
